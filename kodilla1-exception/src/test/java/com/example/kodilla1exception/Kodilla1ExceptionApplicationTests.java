b72792dc61bf58a0c3164cd124864225d9b27579
Anna Kościelecka <akoscielecka18@gmail.com>
2022-12-01 16:47:44 +0100

    8.2 first exception

---
+++ kodilla1-exception/src/test/java/com/example/kodilla1exception/Kodilla1ExceptionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.kodilla1exception;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Kodilla1ExceptionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
