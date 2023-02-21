caa9cc11a6a13ef9f2114ea1ac362d68be79c552
Anna Kościelecka <akoscielecka18@gmail.com>
2023-02-21 13:32:50 +0100

    15.1

--- kodilla-spring-web/src/main/java/com/kodilla/spring/KodillaSpringWebApplication.java
+++ kodilla-spring-web/src/main/java/com/kodilla/spring/KodillaSpringWebApplication.java
@@ -1,5 +1,4 @@
-package com.kodilla.spring.web;
-
+package com.kodilla.spring;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -9,5 +8,4 @@ public class KodillaSpringWebApplication {
     public static void main(String[] args) {
         SpringApplication.run(KodillaSpringWebApplication.class, args);
     }
-
 }
